1d2896aeaae62799f06a4349cddcdbddcaf037aa
Chris Chronopoulos <chronopoulos.chris@gmail.com>
2014-04-29 02:17:49 -0400

    first pass soundpuddle2.py

--- soundpuddle/soundpuddle2.py
+++ soundpuddle/soundpuddle2.py
@@ -31,6 +31,15 @@ class SoundPuddle():
             self.spokes.append([-1 for j in range(20)])
         self.launchpad = [[0x80,0x80,0x80]]*8
 
+    def colorMap(self,value):
+        diff = value - self.sensitivity
+        if diff<1.:
+            return [0x80,0xFF,0x80] # red
+        elif diff<5.:
+            return [0xFF,0x80,0x80] # green
+        else:
+            return [0x80,0x80,0xFF] # blue
+
     def handleOSC(self, pathstr, arg, typestr, server, usrData):
         for i in range(8):
             if arg[i] >= self.sensitivity:
@@ -39,23 +48,10 @@ class SoundPuddle():
             else:
                 self.launchpad[i] = [0x80,0x80,0x80]
 
-    def colorMap(self,value):
-        diff = value - self.sensitivity
-        if diff<
-
     def shiftSpokes(self):
         for i in range(4):
             self.buff[(60*2*i):(60*2*i+60)] = self.launchpad[2*i] + self.buff[(60*2*i):(60*2*i+57)]
             self.buff[(60*(2*i+1)):(60*(2*i+1)+60)] = self.buff[(60*(2*i+1)):(60*(2*i+1)+57)] + self.launchpad[2*i+1]
-
-    def prepareBuffer(self):
-        for i in range(8):
-            for j in range(20):
-                if self.spokes[i][j]==1:
-                    #self.buff[(i*20+j)*3:(i*20+j)*3+3] = random.choice(self.colorSet)
-                    self.buff[(i*20+j)*3:(i*20+j)*3+3] = [0xFF,0xFF,0xFF]
-                else:
-                    self.buff[(i*20+j)*3:(i*20+j)*3+3] = [0x80,0x80,0x80]
 
     def writeBuffer(self):
         spidev.write(self.buff+self.zeros)
@@ -65,7 +61,6 @@ class SoundPuddle():
         while True:
             self.OSCserver.recv(1)
             self.shiftSpokes()
-            self.prepareBuffer()
             self.writeBuffer()
 
 if __name__=='__main__':
