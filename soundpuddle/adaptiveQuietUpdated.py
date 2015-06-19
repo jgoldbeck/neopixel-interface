23f40a1900734726d0cd85c81d89c0bc6f86a045
Joseph Goldbeck <goldbeck.joseph@gmail.com>
2015-06-19 01:21:26 -0400

    Revert be3d0a1..afa20ec
    This rolls back to commit be3d0a107e3641aaf813d39fc01b3041ab7cae10.

--- soundpuddle/adaptiveQuietUpdated.py
+++ soundpuddle/adaptiveQuietUpdated.py
@@ -90,23 +90,19 @@ class TwistedPuddle(object):
                     self.launchpad[i%8] = self.colorMap(3*(value - threshold))
                 self.adaptiveThreshold[i] = max(threshold - .02, value)
 
-
     def shiftSpokes(self):
 
 
         ## Down?
         #
-        print 'ya'
-        self.circ = 20
-        print self.nleds / self.circ
-        for i in range(1, self.nleds / self.circ):
-            print i
-            self.buff[i*self.circ: (i+1)*self.circ] = self.buff[(i-1)*self.circ: (i)*self.circ]
+        # self.circ = 20;
+        # print self.nleds / self.circ
+        # for i in range(1, self.nleds / self.circ):
+        #     print i
+        #     self.buff[i*self.circ: (i+1)*self.circ] = self.buff[(i-1)*self.circ: (i)*self.circ]
+        # self.buff[0:8] = self.launchpad
 
-        for j in range(0, 8):
-            self.buff[j] = self.launchpad[j]
-
-            return
+        # return
 
 
         # Multiple spokes
@@ -152,7 +148,7 @@ class TwistedPuddle(object):
             column[y3]     = gamma[value[1]]
             column[y3 + 1] = gamma[value[0]]
             column[y3 + 2] = gamma[value[2]]
-            #print (column[y3],column[y3+1],column[y3+2])
+            print (column[y3],column[y3+1],column[y3+2])
 
         return column
 
